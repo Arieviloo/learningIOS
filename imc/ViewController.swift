60171ac434ebfa8c296d2f100e7825b03afdc8e6
Jadië Oliveira <jadieoliveira00@gmail.com>
2021-01-09 20:30:24 -0300

    init

--- imc/ViewController.swift
+++ imc/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfWeight: UITextField!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var ivResult: UIImageView!
+    @IBOutlet weak var viewResult: UIView!
+    var imc: Double = 0
+    
+    @IBOutlet weak var tfHeight: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculate(_ sender: Any) {
+        if let weight = Double(tfWeight.text!), let height = Double(tfHeight.text!) {
+            imc = weight / pow(height, 2)
+            showResult()
+        }
+    }
+    
+    func showResult() {
+        var result: String = ""
+        var img: String = ""
+        
+        switch imc {
+            case 0..<16:
+                result = "Magreza"
+                img = "abaixo"
+            case 16..<18.5:
+                result = "Abaixo do peso"
+                img = "abaixo"
+            case 18.5..<25:
+                result = "Ideal"
+                img = "ideal"
+            case 25..<30:
+                result = "Sobrepeso"
+                img = "sobre"
+            default:
+                result = "Obesidade"
+                img = "obesidade"
+        }
+        lbResult.text = "\(Int(imc)) : \(result)"
+        ivResult.image = UIImage(named: img)
+        viewResult.isHidden = false
+    }
+    
 }
 
